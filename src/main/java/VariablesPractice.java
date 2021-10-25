45a99cb736ef38f339749ec1414b3a89c9a3d697
Mandell Butler <mandell@mandells-mbp.lan>
2021-10-25 14:58:49 -0400

    Constructed User Post Comment and Vote models

---
+++ src/main/java/VariablesPractice.java
@@ -0,0 +1,11 @@
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int him = 2;
+        double her = 10.222;
+        double sum = him + her;
+        String name = "mandell";
+        String result = him + name;
+            System.out.println(sum);
+            System.out.println(result);
+    }
+}
